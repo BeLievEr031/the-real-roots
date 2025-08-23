cb117e87a30fed26486932f416630cf7fa2dc9d1
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-23 22:17:47 +0530

    feat: header mobile responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import HeaderImg from "../assets/header.svg"
 function Header() {
     return (
-        <div className="py-4 flex justify-center items-center gap-5 bg-purple-50 border-b-[0.5px]">
-            <img src={HeaderImg} alt={HeaderImg} />
+        <div className="px-2 py-4 flex justify-between md:justify-center items-center md:gap-5 bg-purple-50 border-b-[0.5px]">
+            <img src={HeaderImg} alt={HeaderImg} className="md:inline hidden" />
             <div>
                 <h1 className="font-semibold">
                     What's your social vibe? ✨
                 </h1>
-                <p>
+                <p className="hidden md:block">
                     Take a fun & free personality quiz to find out!
                 </p>
             </div>
