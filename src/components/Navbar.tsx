ce4dba54a183ee587b99250d3bd4e557a8d3fe48
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-24 17:21:50 +0530

    feat: add mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom"
 import LogoImg from "../assets/logo.svg"
 import NavLink from "./Link"
+import { Menu, X } from "lucide-react"
+import clsx from "clsx"
+import { useState } from "react"
 
 
 const linksArr = [
@@ -24,17 +27,35 @@ const linksArr = [
     },
     {
         label: "Partner with us",
-        href: "partners "
+        href: "partners"
 
     },
 ]
 function Navbar() {
+    const [open, setOpen] = useState(false)
     return (
-        <nav className="px-20 py-4 flex justify-between items-center">
+        <nav className={
+            clsx("relative px-4 md:px-20 py-4 flex justify-between items-center",)
+        }>
             <Link to={"/"}>
-                <img src={LogoImg} alt="logo-img" className="w-[150px] hidden md:inline" />
+                <img src={LogoImg} alt="logo-img" className="w-[150px] inline" />
             </Link>
             <div className="md:flex gap-8 hidden">
+                {
+                    linksArr.map((item) => {
+                        return <NavLink label={item.label} href={item.href} />
+                    })
+                }
+            </div>
+            <button onClick={() => setOpen(!open)} >
+                {!open ? <Menu /> : <X />}
+            </button>
+
+            <div className={
+                clsx("absolute top-0 left-0 w-full transition-all h-screen bg-white md:hidden -translate-y-full flex flex-col opacity-0 pointer-none pt-20 items-center duration-300", {
+                    'translate-y-0 top-full opacity-100': open
+                })
+            }>
                 {
                     linksArr.map((item) => {
                         return <NavLink label={item.label} href={item.href} />
